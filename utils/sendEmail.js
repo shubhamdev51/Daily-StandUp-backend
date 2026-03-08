f1325d2be92ff2d44871544ff742f87976a0ed5d
shubhamdev51 <shubham04.jsr@gmail.com>
2026-03-09 04:21:22 +0530

    sendEmail file changes

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,46 +1,21 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.BREVO_EMAIL,
-    pass: process.env.BREVO_SMTP_KEY
-  }
-});
+const emailjs = require("@emailjs/nodejs");
 
 const sendEmail = async (email, otp) => {
   try {
-
-    const mailOptions = {
-      from: "yourbrevoemail@gmail.com",
-      to: email,
-      subject: "Your OTP for Authentication",
-      html: `<div style="font-family: Arial, sans-serif; background-color:#f4f6f8;padding:30px">
-        <div style="max-width:500px;margin:auto;background:#fff;padding:30px;border-radius:8px">
-          <h2 style="text-align:center">Email Verification</h2>
-          <p>Your One Time Password is:</p>
-
-          <div style="text-align:center;margin:30px 0">
-            <span style="font-size:28px;font-weight:bold;letter-spacing:6px">
-              ${otp}
-            </span>
-          </div>
-
-          <p style="text-align:center;color:gray">
-            This OTP will expire in 5 minutes.
-          </p>
-        </div>
-      </div>`
-    };
-
-    const info = await transporter.sendMail(mailOptions);
-
-    console.log("Email sent:", info.messageId);
-
+    const response = await emailjs.send(
+      process.env.EMAILJS_SERVICE_ID,
+      process.env.EMAILJS_TEMPLATE_ID,
+      {
+        email: email,
+        otp: otp
+      },
+      {
+        publicKey: process.env.EMAILJS_PUBLIC_KEY
+      }
+    );
+    console.log("Email sent:", response);
   } catch (error) {
-    console.error("Email sending failed:", error);
+    console.error("Email failed:", error);
     throw error;
   }
 };
